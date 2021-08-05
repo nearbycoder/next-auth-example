af12b20fbb796f2cefc9a7e32de1c1bc479733ee
Josh Hamilton <nearbycoder@gmail.com>
2021-08-04 22:14:31 -0500

    add api for post

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import { signIn, signOut, getSession } from 'next-auth/client';
 
 export default function Page({ session }) {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
   return (
     <>
       {!session && (
@@ -12,6 +16,36 @@ export default function Page({ session }) {
       {session && (
         <>
           Signed in as {session.user.email} <br />
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+
+              fetch('/api/posts', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ title, body }),
+              });
+            }}>
+            <div>
+              <label>
+                Title:
+                <input
+                  onChange={(e) => setTitle(e.target.value)}
+                  name="title"></input>
+              </label>
+            </div>
+            <div>
+              <label>
+                Body:
+                <textarea
+                  onChange={(e) => setBody(e.target.value)}
+                  name="body"></textarea>
+              </label>
+            </div>
+            <button>Submit</button>
+          </form>
           <button onClick={() => signOut()}>Sign out</button>
         </>
       )}
---
+++ prisma/migrations/20210805025439_add_post_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
